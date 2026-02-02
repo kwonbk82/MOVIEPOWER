7ed7c81bdfc19736780d270991d0fed4aa698303
kwonbk82 <zkbk4982@gmail.com>
2026-02-02 15:41:32 +0900

    해치웠나?

---
+++ back/src/main/java/com/power/movie_ranger/MovieRangerApplication.java
@@ -0,0 +1,13 @@
+package com.power.movie_ranger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRangerApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/power/movie_ranger/MovieRangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.power.movie_ranger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
